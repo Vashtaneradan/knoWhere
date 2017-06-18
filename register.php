37df4776cb275b400a535623c15df9438a4a697a
Olmancketslat <Olmancketslat@users.noreply.github.com>
2017-06-18 16:50:11 +0200

    Updated register.php with labels and header/body
    added header & body structure
    added labels for desired username and password
    added required attribute into register form
    changed title from Login to Registrieren
    added starting of session

--- register.php
+++ register.php
@@ -1,10 +1,26 @@
-<!--Formular für das registrieren -->
-<div id="registerwrapper gamebody-background">
-    <h1>Register</h1>
-    <form name="register" method="post" action="register-action.php" id="register">
-        <input type="text" name="username" id="username" placeholder="Username">
-        <input type="password" name="password" id="password" placeholder="Password">
-        <input type="submit" value="Register" name="btnregister" id="btnregister">
-    </form>
-    <p><a href="index.php?page=login">oder zurück zum Login</a></p>
-</div>
+<?php
+session_start();
+?>
+<!DOCTYPE  html>
+<html lang="de">
+
+    <head>
+	    <meta charset="UTF-8">
+	    <title>Registrieren</title>
+    </head>
+    
+    <body>
+        <!--Formular für das registrieren -->
+        <div id="registerwrapper gamebody-background">
+            <h1>Register</h1>
+            <form name="register" method="post" action="register-action.php" id="register">
+                <label for="username">Wunsch-Benutzername:</label>
+                <input type="text" name="username" id="username" placeholder="Username">
+                <label for="password">Passwort:</label>
+                <input type="password" name="password" id="password" placeholder="Password">
+                <input type="submit" value="Register" name="btnregister" id="btnregister">
+            </form>
+            <p><a href="index.php?page=login">oder zurück zum Login</a></p>
+        </div>
+    </body>
+</html>
